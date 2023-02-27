ea99ff469c9608459bca6723a059ff0e5ac5196c
Kazik6maziK <dimaredlin715@gmail.com>
2023-02-27 21:19:38 +0300

    fix: task human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+  const oneToNine = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight','nine'];
+  const elevenTonineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tensese = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  let resalt = ''
+
+  if (number < 10) {
+    resalt = oneToNine[number];
+  } else if (number < 20) {
+    resalt = elevenTonineteen[number - 10];
+  } else if (number < 100) {
+    resalt = `${tensese[Math.floor(number / 10)]} ${number % 10 > 0 ? toReadable(number % 10) : ''}`;
+  } else if (number < 1000) {
+    resalt = `${oneToNine[Math.floor(number / 100)]} hundred ${number % 100 > 0 ? toReadable(number % 100) : ''}`;
+  }
+
+  return resalt.trim();
 }
+
